1c2dc4cbb805557f2469d1711c8ae97ffcc3509f
jvanderwee <joe@jvanderwee.com>
2014-12-28 08:52:53 +0000

    Added check for SAUCE_USERNAME and SAUCE_ACCESS_KEY env vars

--- src/test/java/com/bootstrapium/config/Sauce.java
+++ src/test/java/com/bootstrapium/config/Sauce.java
@@ -18,10 +18,14 @@ public class Sauce {
     private static final SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(
             System.getenv("SAUCE_USERNAME"), System.getenv("SAUCE_ACCESS_KEY"));
 
-    private static final SauceREST client = new SauceREST(authentication.getUsername(),
-            authentication.getAccessKey());
-    
+    private static final SauceREST client = new SauceREST(
+            authentication.getUsername(), authentication.getAccessKey());
+
     public static URL getURL() throws MalformedURLException {
+        if(!environmentVariablesSet()) {
+            throw new RuntimeException("SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables must be set.");
+        }
+        
         return new URL(String.format(
                 "http://%s:%s@ondemand.saucelabs.com:80/wd/hub",
                 authentication.getUsername(), authentication.getAccessKey()));
@@ -35,10 +39,17 @@ public class Sauce {
             SauceOnDemandSessionIdProvider sessionIdProvider, String name) {
         Map<String, Object> updates = new HashMap<String, Object>();
         updates.put("name", name);
-        client.updateJobInfo(sessionIdProvider.getSessionId(), updates); 
+        client.updateJobInfo(sessionIdProvider.getSessionId(), updates);
     }
-    
+
     public static void uploadFile(File file) throws IOException {
-    	client.uploadFile(file);
+        client.uploadFile(file);
+    }
+
+    private static boolean environmentVariablesSet() {
+        String username = authentication.getUsername();
+        String accessKey = authentication.getAccessKey();
+        return username != null && !username.isEmpty() && accessKey != null
+                && !accessKey.isEmpty();
     }
 }
