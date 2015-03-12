6a6e78bc6e29fb84ee475da773f2f3bea641e045
Graham <graham@ham1.co.uk>
2015-03-12 13:26:37 +0000

    Guarded against StaleElementReferenceException when checking for Visible elements on a new page

--- src/test/java/com/frameworkium/pages/internal/BasePage.java
+++ src/test/java/com/frameworkium/pages/internal/BasePage.java
@@ -5,6 +5,7 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,6 +24,7 @@ public abstract class BasePage<T extends BasePage<T>> {
     @Inject
     protected WebDriverWait wait;
 
+    /** @return Returns the current page object, useful for 'fluent' tests e.g. MyPage.get().then().doSomething(); */
     @SuppressWarnings("unchecked")
     public T then() {
         return (T) this;
@@ -45,8 +47,7 @@ public abstract class BasePage<T extends BasePage<T>> {
     }
 
     @SuppressWarnings("unchecked")
-    private void waitForVisibleElements(Object pageObject) throws IllegalArgumentException,
-            IllegalAccessException {
+    private void waitForVisibleElements(Object pageObject) throws IllegalArgumentException, IllegalAccessException {
         for (Field field : pageObject.getClass().getDeclaredFields()) {
             for (Annotation annotation : field.getDeclaredAnnotations()) {
                 if (annotation instanceof Visible) {
@@ -65,21 +66,44 @@ public abstract class BasePage<T extends BasePage<T>> {
                     } else if (obj instanceof WebElement) {
                         element = (WebElement) obj;
                     }
-                    wait.until(ExpectedConditions.visibilityOf(element));
+                    // Retries when an element is looked up before the previous page has unloaded or before doc ready
+                    try {
+                        wait.until(ExpectedConditions.visibilityOf(element));
+                    } catch (StaleElementReferenceException serex) {
+                        System.out.println("Caught StaleElementReferenceException");
+                        tryToEnsureWeHaveUnloadedOldPageAndNewPageIsReady();
+                        wait.until(ExpectedConditions.visibilityOf(element));
+                    }
                 }
             }
         }
     }
 
+    private void tryToEnsureWeHaveUnloadedOldPageAndNewPageIsReady() {
+        for (int tries = 0; tries < 3; tries++) {
+            Boolean documentReady = (Boolean) executeJS("return document.readyState == 'complete'");
+            if (!documentReady) {
+                System.out.println("Document not yet ready");
+                tries--;
+            }
+        }
+    }
+
+    /**
+     * @param javascript the Javascript to execute on the current page
+     * @return Returns an Object returned by the Javascript provided
+     */
     public Object executeJS(String javascript) {
         JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
         return jsExecutor.executeScript(javascript);
     }
 
+    /** @return Returns the title of the web page */
     public String getTitle() {
         return driver.getTitle();
     }
 
+    /** @return Returns the source code of the current page */
     public String getSource() {
         return driver.getPageSource();
     }
