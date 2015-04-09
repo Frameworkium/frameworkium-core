23c67334c81333a45f1f8cd903853bc29889b8c5
Graham Russell <graham@ham1.co.uk>
2015-04-09 12:01:06 +0100

    Attempted to implement get node address.

--- src/test/java/com/frameworkium/capture/ScreenshotCapture.java
+++ src/test/java/com/frameworkium/capture/ScreenshotCapture.java
@@ -1,5 +1,8 @@
 package com.frameworkium.capture;
 
+import java.net.InetAddress;
+import java.net.URL;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
@@ -22,14 +25,9 @@ public class ScreenshotCapture {
     private String executionID;
 
     public ScreenshotCapture(String testID, WebDriver webdriver) {
-        String node = "TODO";
-        if (DriverType.useRemoteWebDriver) {
-            RemoteWebDriver r = (RemoteWebDriver) webdriver;
-            // TODO: get remote address etc.
-        }
-        initExecution(new CreateExecution(testID, node));
+        initExecution(new CreateExecution(testID, getNode()));
     }
-
+    
     public void takeAndSendScreenshot(Command command, WebDriver webdriver, String errorMessage) {
 
         String url = DriverType.isNative() ? "" : webdriver.getCurrentUrl();
@@ -46,6 +44,10 @@ public class ScreenshotCapture {
                 .then().log().headers().statusCode(201);
     }
 
+    public static boolean isRequired() {
+        return SystemProperty.CAPTURE_URL.isSpecified() && !DriverType.isNative();
+    }
+
     private void initExecution(CreateExecution createExecutionMessage) {
 
         String uri = SystemProperty.CAPTURE_URL.getValue() + "/execution";
@@ -55,8 +57,27 @@ public class ScreenshotCapture {
         logger.info("executionID = " + executionID);
     }
 
-    public static boolean isRequired() {
-        return SystemProperty.CAPTURE_URL.isSpecified() && !DriverType.isNative();
+    private String getNode() {
+
+        String node = "n/a";
+        if (DriverType.useRemoteWebDriver) {
+            try {
+                RemoteWebDriver r = (RemoteWebDriver) webdriver;
+                URL gridURL = new URL(SystemProperty.GRID_URL);
+                String url = gridURL.getProtocol() + gridURL.getHost() + ':' + gridURL.getPort() 
+                                + "/grid/api/testsession?session=" + r.getSessionId();
+                node = RestAssured.post(url).andReturn().jsonPath().getString("proxyId");
+            } catch (Throwable t) {
+                logger.warn("Failed to get node address of remote web driver", t);
+            }
+        } else {
+            try {
+                node = InetAddress.getLocalHost().getCanonicalHostName();
+            } catch (UnknownHostException e) {
+                logger.warn("Failed to get local machine name", e);
+            }
+        }
+        return node;
     }
 
 }
