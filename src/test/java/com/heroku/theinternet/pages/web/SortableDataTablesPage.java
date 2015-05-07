9ce597d6e4e5c02d2bd62ab7c2391f617f64d330
robertgates55 <robertgates55@gmail.com>
2015-05-07 17:29:40 +0100

    Added steps to sortabledatatables example

--- src/test/java/com/heroku/theinternet/pages/web/SortableDataTablesPage.java
+++ src/test/java/com/heroku/theinternet/pages/web/SortableDataTablesPage.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.htmlelements.annotations.Name;
 import ru.yandex.qatools.htmlelements.element.Table;
 
@@ -29,32 +30,33 @@ public class SortableDataTablesPage extends BasePage<SortableDataTablesPage> {
     @FindBy(css = "table#table2")
     private Table table2;
 
+    
+    @Step("Get table 1 column '{0}' contents")
     public List<String> getTable1ColumnContents(String colHeader) {
         return getColumnContents(table1, colHeader);
     }
     
+    @Step("Get table 2 column '{0}' contents")
     public List<String> getTable2ColumnContents(String colHeader) {
         return getColumnContents(table2, colHeader);
     }
     
-    public SortableDataTablesPage sortTable2ByColumnName(String colHeader) {
-        
+    @Step("Sort table 2 by column with header '{0}'")
+    public SortableDataTablesPage sortTable2ByColumnName(String colHeader) { 
         sortTableByColumnName(table2, colHeader);
         return this;
-
     }
     
+    @Step("Sort table ({0}) by column name '{1}'")
     private void sortTableByColumnName(Table table, String colHeader) {
-        
         table.getHeadings().get(table.getHeadingsAsString().indexOf(colHeader)).click();
     }
     
+    @Step("Get column contents of column '{1}' in table '{0}'")
     private List<String> getColumnContents(Table table, String colHeader) {
-        
         //We should be able to use the built in methods to achieve this, but
         //because the table contains a blank row, the methods fail.
         //Thus, some manipulation is required:
- 
         int a = table.getHeadingsAsString().indexOf(colHeader);
         
         List<String> columnContents = new ArrayList<String>();
