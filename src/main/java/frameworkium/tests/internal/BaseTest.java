93cbbce7991ed523bfb0fb77d139b553f5830cbc
robertgates55 <robertgates55@gmail.com>
2015-08-14 12:21:57 +0100

    Refactored to just contain the core frameworkium code. Removed the example tests.

--- src/main/java/frameworkium/tests/internal/BaseTest.java
+++ src/main/java/frameworkium/tests/internal/BaseTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
