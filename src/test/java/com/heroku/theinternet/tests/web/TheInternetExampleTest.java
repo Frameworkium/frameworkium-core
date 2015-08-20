8b93191a13b29e8ebc647cc107e541d94981723e
jleeh <JonnyH@outlook.com>
2015-08-20 11:40:18 +0100

    Re-added the example, basic test.

---
+++ src/test/java/com/heroku/theinternet/tests/web/TheInternetExampleTest.java
@@ -0,0 +1,17 @@
+package com.heroku.theinternet.tests.web;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.frameworkium.tests.internal.BaseTest;
+import com.heroku.theinternet.pages.web.WelcomePage;
+
+import org.testng.annotations.Test;
+
+public class TheInternetExampleTest extends BaseTest {
+
+    @Test
+    public void basicAuth() {
+
+        assertThat(WelcomePage.open().then().getTitle()).isEqualTo("The Internet");
+    }
+}
