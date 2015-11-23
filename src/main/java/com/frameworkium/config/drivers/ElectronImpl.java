e9c7d2a88130b7e4cf4ee92930b3978e3358a704
jleeh <JonnyH@outlook.com>
2015-11-23 11:11:37 +0000

    Updated the electron driver so it uses the gridURL which fixes the issues with electron. If gridURL isn't specified, it defaults to localhost.

--- src/main/java/com/frameworkium/config/drivers/ElectronImpl.java
+++ src/main/java/com/frameworkium/config/drivers/ElectronImpl.java
@@ -2,33 +2,43 @@ package com.frameworkium.config.drivers;
 
 import com.frameworkium.config.DriverType;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.frameworkium.config.SystemProperty.APP_PATH;
+import static com.frameworkium.config.SystemProperty.GRID_URL;
 
 public class ElectronImpl extends DriverType {
 
+    private static URL remoteURL;
+
     @Override
     public DesiredCapabilities getDesiredCapabilities() {
-
-        ChromeOptions options = new ChromeOptions();
+        Map<String, String> chromeOptions = new HashMap<>();
         if (!APP_PATH.isSpecified()) {
             logger.error("App path must be specified when using Electron!");
         } else {
-            options.setBinary(APP_PATH.getValue());
+            chromeOptions.put("binary", APP_PATH.getValue());
         }
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities().chrome();
-        desiredCapabilities.setCapability("host", "localhost");
-        desiredCapabilities.setCapability("port", 9515);
-        desiredCapabilities.setCapability("chromeOptions", options);
+        try {
+            remoteURL = new URL(GRID_URL.getValue());
+        }
+        catch(MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("browserName", "chrome");
+        desiredCapabilities.setCapability("chromeOptions", chromeOptions);
         return desiredCapabilities;
     }
 
     @Override
     public WebDriver getWebDriverObject(DesiredCapabilities capabilities) {
-        return new ChromeDriver(capabilities);
+        return new RemoteWebDriver(remoteURL ,capabilities);
     }
-
 }
