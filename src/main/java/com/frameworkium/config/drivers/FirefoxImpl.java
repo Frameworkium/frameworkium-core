ce2e986d05f444bb8463480f6fd5e68f8fab551e
JHuxtable <jonny.huxtable@baesystems.com>
2016-01-15 15:43:04 +0000

    Added the ability to use a file as a Firefox Profile, along with a profile name

--- src/main/java/com/frameworkium/config/drivers/FirefoxImpl.java
+++ src/main/java/com/frameworkium/config/drivers/FirefoxImpl.java
@@ -3,8 +3,11 @@ package com.frameworkium.config.drivers;
 import com.frameworkium.config.DriverType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
 
 import static com.frameworkium.config.SystemProperty.FIREFOX_PROFILE;
 
@@ -18,7 +21,15 @@ public class FirefoxImpl extends DriverType {
     @Override
     public WebDriver getWebDriverObject(DesiredCapabilities capabilities) {
         if (FIREFOX_PROFILE.isSpecified()) {
-            return new FirefoxDriver(new ProfilesIni().getProfile(FIREFOX_PROFILE.getValue()));
+            File profileFile = new File(FIREFOX_PROFILE.getValue());
+            FirefoxProfile firefoxProfile;
+            if (profileFile.isFile() && !profileFile.isDirectory()) {
+                firefoxProfile = new FirefoxProfile(profileFile);
+            }
+            else {
+                firefoxProfile = new ProfilesIni().getProfile(FIREFOX_PROFILE.getValue());
+            }
+            return new FirefoxDriver(firefoxProfile);
         }
         else {
             return new FirefoxDriver(capabilities);
