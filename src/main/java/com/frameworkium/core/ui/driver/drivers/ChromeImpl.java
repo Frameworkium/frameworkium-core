0f266a77bb462078fecf74295f4aa6e8415d1184
Graham Russell <graham@ham1.co.uk>
2016-05-23 13:15:36 +0100

    minor refactor of ChromeImpl

--- src/main/java/com/frameworkium/core/ui/driver/drivers/ChromeImpl.java
+++ src/main/java/com/frameworkium/core/ui/driver/drivers/ChromeImpl.java
@@ -2,6 +2,7 @@ package com.frameworkium.core.ui.driver.drivers;
 
 import com.frameworkium.core.common.properties.Property;
 import com.frameworkium.core.ui.driver.DriverType;
+import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,25 +10,26 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.*;
 
+import static java.util.Collections.singletonList;
+
 public class ChromeImpl extends DriverType {
 
     @Override
     public DesiredCapabilities getDesiredCapabilities() {
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(
-                "chrome.switches", Arrays.asList("--no-default-browser-check"));
-        HashMap<String, String> chromePreferences = new HashMap<>();
-        chromePreferences.put("profile.password_manager_enabled", "false");
-        capabilities.setCapability("chrome.prefs", chromePreferences);
+                "chrome.switches",
+                singletonList("--no-default-browser-check"));
+        capabilities.setCapability(
+                "chrome.prefs",
+                ImmutableMap.of("profile.password_manager_enabled", "false"));
 
         // Use Chrome's built in device emulators
         // Specify browser=chrome, but also provide device name to use chrome's emulator
         if (Property.DEVICE.isSpecified()) {
-            Map<String, String> mobileEmulation = new HashMap<>();
-            mobileEmulation.put("deviceName", Property.DEVICE.getValue());
-
-            Map<String, Object> chromeOptions = new HashMap<>();
-            chromeOptions.put("mobileEmulation", mobileEmulation);
+            Map<String, Map<String, String>> chromeOptions = ImmutableMap.of(
+                    "mobileEmulation", ImmutableMap.of(
+                            "deviceName", Property.DEVICE.getValue()));
 
             capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
         }
