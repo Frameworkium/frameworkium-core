29405e58bb43571064fb6f3a1d68de79452f39be
Graham Russell <graham@ham1.co.uk>
2016-05-28 15:51:23 +0100

    Removed unneeded Guice injection from ServiceFactory.

--- src/main/java/com/frameworkium/core/api/services/ServiceFactory.java
+++ src/main/java/com/frameworkium/core/api/services/ServiceFactory.java
@@ -1,6 +1,5 @@
 package com.frameworkium.core.api.services;
 
-import com.google.inject.Guice;
 import com.jayway.restassured.http.Method;
 import com.jayway.restassured.specification.RequestSpecification;
 
@@ -8,23 +7,24 @@ public class ServiceFactory {
 
     public static <T extends BaseService<T>> T newInstance(
             Class<T> clazz, String url) {
-        return Guice.createInjector(new ServicesGuiceModule())
-                .getInstance(clazz)
-                .get(url);
+        return newServiceInstance(clazz).get(url);
     }
 
     public static <T extends BaseService<T>> T newInstance(
             Class<T> clazz, String url, RequestSpecification requestSpec) {
-        return Guice.createInjector(new ServicesGuiceModule())
-                .getInstance(clazz)
-                .get(url, requestSpec);
+        return newServiceInstance(clazz).get(url, requestSpec);
     }
 
     public static <T extends BaseService<T>> T newInstance(
             Class<T> clazz, String url, Method httpVerb, RequestSpecification requestSpec) {
-        return Guice.createInjector(new ServicesGuiceModule())
-                .getInstance(clazz)
-                .get(url, httpVerb, requestSpec);
+        return newServiceInstance(clazz).get(url, httpVerb, requestSpec);
     }
 
+    private static <T extends BaseService<T>> T newServiceInstance(Class<T> clazz) {
+        try {
+            return clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Unable to instantiate ServiceObject", e);
+        }
+    }
 }
