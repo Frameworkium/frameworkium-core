18b1c7a1ca7cfd1a25b7ba70e0e3e56e28cfc36f
robertgates55 <robertgates55@gmail.com>
2016-12-08 15:29:42 +0000

    Added customImpl for travis test

---
+++ src/test/java/com/frameworkium/integration/customImpl.java
@@ -0,0 +1,20 @@
+package com.frameworkium.integration;
+
+import com.frameworkium.core.ui.driver.AbstractDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class customImpl extends AbstractDriver {
+
+    @Override
+    public DesiredCapabilities getDesiredCapabilities() {
+        return DesiredCapabilities.firefox();
+    }
+
+    @Override
+    public WebDriver getWebDriver(DesiredCapabilities capabilities) {
+            return new FirefoxDriver(capabilities);
+    }
+
+}
