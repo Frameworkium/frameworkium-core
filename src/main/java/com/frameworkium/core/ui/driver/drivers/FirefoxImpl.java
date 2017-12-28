f0f7b23eb89a364fdbbf4384ae6f90738ea9d0b4
Graham Russell <graham@ham1.co.uk>
2017-12-28 10:52:18 +0800

    Updated to Selenium 3.8.1 and removed firefoxProfile property (#91)
    * Updated to Selenium 3.8.1
    * Removed firefoxProfile property - use -Dwebdriver.firefox.profile instead
    * Travis: reduced memory of grid and mvn to give browsers more space

--- src/main/java/com/frameworkium/core/ui/driver/drivers/FirefoxImpl.java
+++ src/main/java/com/frameworkium/core/ui/driver/drivers/FirefoxImpl.java
@@ -1,14 +1,9 @@
 package com.frameworkium.core.ui.driver.drivers;
 
-import com.frameworkium.core.common.properties.Property;
 import com.frameworkium.core.ui.driver.AbstractDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.io.File;
 
 public class FirefoxImpl extends AbstractDriver {
 
@@ -19,18 +14,6 @@ public class FirefoxImpl extends AbstractDriver {
 
     @Override
     public WebDriver getWebDriver(DesiredCapabilities capabilities) {
-        if (Property.FIREFOX_PROFILE.isSpecified()) {
-            FirefoxProfile firefoxProfile;
-            String fireFoxProfile = Property.FIREFOX_PROFILE.getValue();
-            File profileFile = new File(fireFoxProfile);
-            if (profileFile.isFile() || profileFile.isDirectory()) {
-                firefoxProfile = new FirefoxProfile(profileFile);
-            } else {
-                firefoxProfile = new ProfilesIni().getProfile(fireFoxProfile);
-            }
-            return new FirefoxDriver(firefoxProfile);
-        } else {
-            return new FirefoxDriver(capabilities);
-        }
+        return new FirefoxDriver(capabilities);
     }
 }
