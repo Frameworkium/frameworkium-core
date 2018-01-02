8d4333765526535a8e92292c35a589a9e1b5bdb3
Graham Russell <graham@ham1.co.uk>
2018-01-02 16:02:15 +0800

    Deprecate BaseTest's and added BaseAPITest and BaseUITest

---
+++ src/main/java/com/frameworkium/core/api/tests/BaseAPITest.java
@@ -0,0 +1,23 @@
+package com.frameworkium.core.api.tests;
+
+import com.frameworkium.core.common.listeners.*;
+import com.frameworkium.core.common.reporting.allure.AllureProperties;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Listeners;
+
+@Listeners({MethodInterceptor.class,
+        TestListener.class,
+        ResultLoggerListener.class})
+public abstract class BaseAPITest {
+
+    protected final Logger logger = LogManager.getLogger(this);
+
+    /** Creates the allure properties for the report, after the test run. */
+    @AfterSuite(alwaysRun = true)
+    public static void createAllureProperties() {
+        AllureProperties.create();
+    }
+
+}
