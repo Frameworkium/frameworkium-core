89117c8777ce629478c4e3e0668fe405b6741d08
Graham Russell <graham@ham1.co.uk>
2018-01-06 19:47:21 +0800

    Tidy up Command

--- src/main/java/com/frameworkium/core/ui/capture/model/Command.java
+++ src/main/java/com/frameworkium/core/ui/capture/model/Command.java
@@ -2,6 +2,7 @@ package com.frameworkium.core.ui.capture.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.frameworkium.core.ui.driver.Driver;
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebElement;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -11,27 +12,24 @@ public class Command {
     public String using;
     public String value;
 
-    /**
-     * Create Command object.
-     */
     public Command(String action, String using, String value) {
         this.action = action;
         this.using = using;
         this.value = value;
     }
 
-    /**
-     * Create Command object.
-     */
     public Command(String action, WebElement element) {
         this.action = action;
+        setUsingAndValue(element);
+    }
 
+    private void setUsingAndValue(WebElement element) {
         // TODO: Improve this. Use hacky solution in LoggingListener?
         if (Driver.isNative()) {
             this.using = "n/a";
             this.value = "n/a";
         } else {
-            if (!(element.getAttribute("id")).isEmpty()) {
+            if (StringUtils.isNotBlank(element.getAttribute("id"))) {
                 this.using = "id";
                 this.value = element.getAttribute("id");
             } else if (!(element.getText()).isEmpty()) {
