8fd4a344b6483ac4924a5df8ce41e3368c2a9542
Graham Russell <graham@ham1.co.uk>
2018-09-05 22:40:01 +0800

    Simplify ComponentExampleTest and use correct Issue/TmsLink annotations.

--- src/test/java/com/frameworkium/integration/github/tests/ComponentExampleTest.java
+++ src/test/java/com/frameworkium/integration/github/tests/ComponentExampleTest.java
@@ -1,8 +1,8 @@
 package com.frameworkium.integration.github.tests;
 
 import com.frameworkium.core.ui.tests.BaseUITest;
-import com.frameworkium.integration.github.pages.ExplorePage;
 import com.frameworkium.integration.github.pages.HomePage;
+import io.qameta.allure.Issue;
 import io.qameta.allure.TmsLink;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -25,21 +25,18 @@ public class ComponentExampleTest extends BaseUITest {
     @Test(description = "Simple test showing the use of components")
     public final void component_example_test() {
 
-        // Navigate to homepage then use the nav bar to go to the explore page
-        ExplorePage explorePage = HomePage.open().then().with().theHeader().clickExplore();
-
-        // not a great assertion, improving this is an exercise for the reader
-        assertThat(explorePage.getTitle()).isEqualTo("Explore · GitHub");
-
         // Search for "Selenium" and check SeleniumHQ/selenium is one of the returned repos.
-        List<String> searchResults = explorePage.with().theHeader()
+        List<String> searchResults = HomePage.open()
+                .with().theHeader()
+                .clickExplore()
+                .with().theHeader()
                 .search("Selenium")
                 .getRepoNames();
 
         assertThat(searchResults).contains("SeleniumHQ/selenium");
     }
 
-    @TmsLink("Force Visible")
+    @Issue("Force Visible")
     @Test(description = "force visible makes hidden element visible")
     public void force_visible_makes_hidden_element_visible() {
         HomePage.open()
@@ -47,7 +44,7 @@ public class ComponentExampleTest extends BaseUITest {
                 .testForceVisible();
     }
 
-    @TmsLink("Wait update")
+    @Issue("Wait update")
     @Test(dependsOnMethods = {"force_visible_makes_hidden_element_visible"})
     public void ensure_BaseUITest_wait_is_updated_after_browser_reset() {
         // tests bug whereby BasePage.wait wasn't updated after browser reset
