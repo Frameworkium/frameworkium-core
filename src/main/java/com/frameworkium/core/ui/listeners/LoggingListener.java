c695446b83caf903f9dccd4ba41ca52acc414a7b
Graham Russell <graham@ham1.co.uk>
2018-09-08 23:40:09 +0800

    Use lambdas for logging to prevent unnecessary evaluation of code.

--- src/main/java/com/frameworkium/core/ui/listeners/LoggingListener.java
+++ src/main/java/com/frameworkium/core/ui/listeners/LoggingListener.java
@@ -13,19 +13,28 @@ public class LoggingListener implements WebDriverEventListener {
 
     private static final Logger logger = LogManager.getLogger();
 
+    private static String getLocatorFromElement(WebElement element) {
+        String str = element.toString();
+        Pattern p = Pattern.compile("->\\s(.*)(?=\\])");
+        Matcher m = p.matcher(str);
+        return m.find() && m.groupCount() > 0
+                ? m.group(1)
+                : str;
+    }
+
     @Override
     public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysSent) {
-        logger.debug("changed value of element with " + getLocatorFromElement(element));
+        logger.debug(() -> "changed value of element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void afterClickOn(WebElement element, WebDriver driver) {
-        logger.debug("clicked element with " + getLocatorFromElement(element));
+        logger.debug(() -> "clicked element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void afterFindBy(By by, WebElement element, WebDriver driver) {
-        logger.debug("found element " + by);
+        logger.debug("found element {}", by);
     }
 
     @Override
@@ -50,14 +59,14 @@ public class LoggingListener implements WebDriverEventListener {
 
     @Override
     public void afterNavigateTo(String url, WebDriver driver) {
-        logger.debug("navigated to " + url);
+        logger.debug("navigated to {}", url);
     }
 
     @Override
     public void afterScript(String script, WebDriver driver) {
         // Only log part of a long script
         // We already log the entire script in beforeScript
-        logger.debug("ran script " + StringUtils.abbreviate(script, 128));
+        logger.debug(() -> "ran script " + StringUtils.abbreviate(script, 128));
     }
 
     @Override
@@ -72,17 +81,17 @@ public class LoggingListener implements WebDriverEventListener {
 
     @Override
     public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
-        logger.debug("change value of element with " + getLocatorFromElement(element));
+        logger.debug(() -> "change value of element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void beforeClickOn(WebElement element, WebDriver driver) {
-        logger.debug("click element with " + getLocatorFromElement(element));
+        logger.debug(() -> "click element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-        logger.debug("find element " + by);
+        logger.debug("before find element by {}", by);
     }
 
     @Override
@@ -113,31 +122,22 @@ public class LoggingListener implements WebDriverEventListener {
 
     @Override
     public <X> void beforeGetScreenshotAs(OutputType<X> outputType) {
-        logger.trace("Before GetScreenshotAs");
+        logger.trace("Before get screenshot as");
     }
 
     @Override
     public <X> void afterGetScreenshotAs(OutputType<X> outputType, X x) {
-        logger.trace("After GetScreenshotAs");
+        logger.trace("After get screenshot as");
     }
 
     @Override
     public void beforeGetText(WebElement webElement, WebDriver webDriver) {
-        logger.trace("Before GetText");
+        logger.trace("Before get text");
     }
 
     @Override
     public void afterGetText(WebElement webElement, WebDriver webDriver, String s) {
-        logger.trace("After GetText");
-    }
-
-    private String getLocatorFromElement(WebElement element) {
-        String str = element.toString();
-        Pattern p = Pattern.compile("->\\s(.*)(?=\\])");
-        Matcher m = p.matcher(str);
-        return m.find() && m.groupCount() > 0
-                ? m.group(1)
-                : str;
+        logger.trace("After get text");
     }
 
     @Override
