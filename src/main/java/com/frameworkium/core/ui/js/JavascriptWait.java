dac1491914aa5e2234a0797f7800a06621611253
Graham Russell <graham@ham1.co.uk>
2018-09-08 18:24:08 +0800

    Simplify Javascript Framework waiting after updating to ngwebdriver 1.1.4

--- src/main/java/com/frameworkium/core/ui/js/JavascriptWait.java
+++ src/main/java/com/frameworkium/core/ui/js/JavascriptWait.java
@@ -1,14 +1,10 @@
 package com.frameworkium.core.ui.js;
 
 import com.frameworkium.core.ui.ExtraExpectedConditions;
-import com.frameworkium.core.ui.js.framework.Angular;
-import com.frameworkium.core.ui.js.framework.AngularTwo;
+import com.paulhammant.ngwebdriver.NgWebDriver;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Wait;
-
-import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
 
 /**
  * Frameworkium implementation of waiting for JS events on page-load.
@@ -17,8 +13,6 @@ public class JavascriptWait {
 
     private final Wait<WebDriver> wait;
     private final JavascriptExecutor javascriptExecutor;
-
-    private AbstractFramework detectedFramework;
 
     public JavascriptWait(WebDriver driver, Wait<WebDriver> wait) {
         this.wait = wait;
@@ -30,63 +24,27 @@ public class JavascriptWait {
      * The following actions are waited for:
      * <ol>
      * <li>Document state to be ready</li>
-     * <li>If page is using a supported JS framework, it will detect and wait</li>
+     * <li>If page is using Angular, it will detect and wait</li>
      * </ol>
      */
     public void waitForJavascriptEventsOnLoad() {
         waitForDocumentReady();
-        detectFramework();
-        waitForJavascriptFramework();
+        waitForAngular();
     }
 
     /**
      * If a page is using a supported JS framework, it will wait until it's ready.
      */
     public void waitForJavascriptFramework() {
-        if (detectedFramework != null) {
-            detectedFramework.waitToBeReady(javascriptExecutor);
-        }
+        waitForAngular();
     }
 
     private void waitForDocumentReady() {
         wait.until(ExtraExpectedConditions.documentBodyReady());
     }
 
-    /**
-     * Assumes, implicitly, only one framework is present.
-     * If there are multiple we will pick an arbitrary one.
-     */
-    private void detectFramework() {
-        detectedFramework = Arrays.stream(SupportedFramework.values())
-                .map(SupportedFramework::getInstance)
-                .filter(framework -> framework.isPresent(javascriptExecutor))
-                .findFirst()
-                .orElse(null);
+    private void waitForAngular() {
+        new NgWebDriver(javascriptExecutor).waitForAngularRequestsToFinish();
     }
 
-    /**
-     * Supported JS Frameworks within Frameworkium and their class which
-     * implement {@link AbstractFramework}.
-     */
-    private enum SupportedFramework {
-        ANGULAR(Angular.class),
-        ANGULAR_TWO(AngularTwo.class);
-
-        private AbstractFramework frameworkInstance;
-
-        SupportedFramework(Class<? extends AbstractFramework> frameworkClass) {
-            try {
-                this.frameworkInstance = frameworkClass
-                        .getDeclaredConstructor()
-                        .newInstance();
-            } catch (InstantiationException | IllegalAccessException
-                    | NoSuchMethodException | InvocationTargetException e) {
-                throw new IllegalStateException(e);
-            }
-        }
-
-        AbstractFramework getInstance() {
-            return frameworkInstance;
-        }
-    }
 }
