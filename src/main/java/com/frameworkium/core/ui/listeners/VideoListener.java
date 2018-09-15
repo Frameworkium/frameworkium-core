0adca1f5d1c1594638b169c03c17f11a984e3b8b
Graham Russell <graham@ham1.co.uk>
2018-09-15 19:13:35 +0800

    Refactored VideoCapture

--- src/main/java/com/frameworkium/core/ui/listeners/VideoListener.java
+++ src/main/java/com/frameworkium/core/ui/listeners/VideoListener.java
@@ -1,26 +1,29 @@
 package com.frameworkium.core.ui.listeners;
 
-import com.frameworkium.core.ui.video.VideoCapture;
 import com.frameworkium.core.ui.tests.BaseUITest;
+import com.frameworkium.core.ui.video.VideoCapture;
 import org.testng.*;
-
-import static com.frameworkium.core.ui.video.VideoCapture.isRequired;
 
 public class VideoListener extends TestListenerAdapter {
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
-        if (isRequired()) {
-            VideoCapture.saveTestSessionID(iTestResult, BaseUITest.getThreadSessionId());
+        if (VideoCapture.isRequired()) {
+            VideoCapture.saveTestSessionID(
+                    iTestResult.getName(),
+                    BaseUITest.getThreadSessionId());
         }
     }
 
     @Override
     public void onFinish(ITestContext iTestContext) {
-        if (isRequired()) {
-            iTestContext.getFailedTests()
+        if (VideoCapture.isRequired()) {
+            iTestContext
+                    .getFailedTests()
                     .getAllResults()
-                    .forEach(VideoCapture::saveVideo);
+                    .stream()
+                    .map(ITestResult::getName)
+                    .forEach(VideoCapture::fetchAndSaveVideo);
         }
     }
 
--- src/main/java/com/frameworkium/core/ui/video/VideoCapture.java
+++ src/main/java/com/frameworkium/core/ui/video/VideoCapture.java
@@ -7,10 +7,10 @@ import org.apache.http.HttpStatus;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.TimeoutException;
-import org.testng.ITestResult;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.file.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,10 +20,10 @@ import static com.frameworkium.core.common.properties.Property.VIDEO_CAPTURE_URL
 
 public class VideoCapture {
 
-    private static final String VIDEO_FOLDER = "capturedVideo";
+    private static final Path VIDEO_FOLDER = Paths.get("capturedVideo");
     private static final Logger logger = LogManager.getLogger();
 
-    private static Map<String, String> testMap = new HashMap<>();
+    private static Map<String, String> testSessionMap = new HashMap<>();
 
     private VideoCapture() {
         // hide default constructor for this util class
@@ -33,42 +33,19 @@ public class VideoCapture {
         return VIDEO_CAPTURE_URL.isSpecified();
     }
 
-    public static void saveTestSessionID(ITestResult iTestResult, String sessionId) {
-        testMap.put(iTestResult.getName(), sessionId);
+    public static void saveTestSessionID(String testName, String sessionId) {
+        testSessionMap.put(testName, sessionId);
     }
 
-    /**
-     * Save video to video folder.
-     */
-    public static void saveVideo(ITestResult iTestResult) {
-        String sessionId = testMap.get(iTestResult.getName());
+    /** Fetch video from remote server and save to video folder. */
+    public static void fetchAndSaveVideo(String testName) {
+        String sessionId = testSessionMap.get(testName);
         URL videoCaptureURL = getVideoCaptureURL(sessionId);
-        byte[] rawVideo;
-        try {
-            rawVideo = getVideo(videoCaptureURL);
-        } catch (InterruptedException | TimeoutException e) {
-            logger.error(String.format(
-                    "Timed out waiting for Session ID %s to become available after 3 retires.",
-                    sessionId));
-            return;
-        }
+        byte[] rawVideo = getVideo(videoCaptureURL);
 
-        Path path = Paths.get(VIDEO_FOLDER);
-        try {
-            if (!Files.exists(path)) {
-                Files.createDirectory(path);
-            }
-            String fileName = String.format(
-                    "%s/%s-%s.%s",
-                    VIDEO_FOLDER,
-                    iTestResult.getName(),
-                    iTestResult.getEndMillis(),
-                    videoCaptureURL.getFile().substring(videoCaptureURL.getFile().lastIndexOf(".") + 1)
-            );
-            Files.write(Paths.get(fileName), rawVideo);
-            logger.info(String.format("Captured video from grid: %s", fileName));
-        } catch (IOException e) {
-            logger.error("Failed creating directory/file for video capture", e);
+        if (rawVideo != null) {
+            String fileName = getFileName(testName, sessionId, videoCaptureURL.getFile());
+            saveVideo(fileName, rawVideo);
         }
     }
 
@@ -76,23 +53,46 @@ public class VideoCapture {
         try {
             return new URL(String.format(VIDEO_CAPTURE_URL.getValue(), sessionId));
         } catch (MalformedURLException e) {
-            throw new RuntimeException("Video Capture URL provided was invalid", e);
+            throw new IllegalArgumentException("Video Capture URL provided was invalid", e);
         }
     }
 
     @Attachment(value = "Video on Failure", type = "video/mp4")
-    private static byte[] getVideo(URL videoCaptureURL)
+    private static byte[] getVideo(URL videoCaptureURL) {
+        try {
+            return fetchVideo(videoCaptureURL);
+        } catch (InterruptedException | TimeoutException e) {
+            logger.error("Failed fetching URL {}.", videoCaptureURL);
+            return null;
+        }
+    }
+
+    private static byte[] fetchVideo(URL videoCaptureURL)
             throws TimeoutException, InterruptedException {
-        int i = 0;
-        while (i++ < 4) {
-            logger.debug("Download URL for Video Capture: " + videoCaptureURL);
+        logger.debug("Download URL for Video Capture: " + videoCaptureURL);
+        for (int i = 0; i < 4; i++) {
             Response response = RestAssured.get(videoCaptureURL);
             if (response.getStatusCode() == HttpStatus.SC_OK) {
                 return response.asByteArray();
             }
-            logger.debug("Retrying download URL for Video Capture: " + videoCaptureURL);
+            logger.debug("Retrying Video Capture download: " + videoCaptureURL);
             TimeUnit.SECONDS.sleep(2);
         }
         throw new TimeoutException();
+    }
+
+    private static String getFileName(String testName, String sessionId, String fileFromUrl) {
+        String fileExt = fileFromUrl.substring(fileFromUrl.lastIndexOf('.') + 1);
+        return String.format("%s-%s.%s", testName, sessionId, fileExt);
+    }
+
+    private static void saveVideo(String fileName, byte[] rawVideo) {
+        try {
+            Files.createDirectories(VIDEO_FOLDER);
+            Files.write(VIDEO_FOLDER.resolve(fileName), rawVideo);
+            logger.info("Captured video: {}", fileName);
+        } catch (IOException e) {
+            logger.error("Failed to save " + fileName, e);
+        }
     }
 }
