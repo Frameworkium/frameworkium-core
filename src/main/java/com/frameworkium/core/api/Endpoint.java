f280d5233de9251ee66d5cc854bb5ea3023f3d94
Graham Russell <graham@ham1.co.uk>
2018-09-26 20:06:15 +0800

    Added documentation to the Endpoint interface.

--- src/main/java/com/frameworkium/core/api/Endpoint.java
+++ src/main/java/com/frameworkium/core/api/Endpoint.java
@@ -1,5 +1,32 @@
 package com.frameworkium.core.api;
 
+/**
+ * Intended for an enum to store the various endpoints of your API under test.
+ *
+ * <p>The following is an example implementation:
+ * <pre>
+ * {@code
+ * public enum MyEndpoint implements Endpoint {
+ *
+ *     BASE_URI("https://xxx),
+ *     YYY_ID("/yyy/%d");
+ *
+ *     private String url;
+ *
+ *     MyEndpoint(String url) {
+ *         this.url = url;
+ *     }
+ *
+ *    @Override
+ *     public String getUrl(Object... params) {
+ *         return String.format(url, params);
+ *     }
+ *  }}
+ *  </pre>
+ *
+ * <p>The key feature is an enum entry for each endpoint where the url String can
+ * contain a {@code String.format} to enable easy injection of parameters.
+ */
 public interface Endpoint {
 
     /**
