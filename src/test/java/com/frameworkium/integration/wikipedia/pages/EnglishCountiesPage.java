066436900d389ae3d089440bd8f49878e57cd5e2
Graham Russell <graham@ham1.co.uk>
2019-08-22 12:08:56 +0100

    Fix Wikipedia test failure.

--- src/test/java/com/frameworkium/integration/wikipedia/pages/EnglishCountiesPage.java
+++ src/test/java/com/frameworkium/integration/wikipedia/pages/EnglishCountiesPage.java
@@ -4,11 +4,13 @@ import com.frameworkium.core.ui.annotations.Visible;
 import com.frameworkium.core.ui.element.OptimisedStreamTable;
 import com.frameworkium.core.ui.pages.BasePage;
 import com.frameworkium.core.ui.pages.PageFactory;
+
 import org.openqa.selenium.NotFoundException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /**
@@ -34,10 +36,11 @@ public class EnglishCountiesPage extends BasePage<EnglishCountiesPage> {
     }
 
     public int populationOf(String countyName) {
-        String lookupHeader = "County";
-        String targetHeader = "Population";
+        Predicate<WebElement> headerLookUp = e -> e.getText().trim().equals("County");
+        Predicate<WebElement> lookUpCellMatcher = e -> e.getText().trim().equals(countyName);
+        Predicate<WebElement> targetColHeaderLookup = e -> e.getText().trim().startsWith("Population");
         String population = listTable
-                .getCellsByLookup(lookupHeader, countyName, targetHeader)
+                .getCellsByLookup(headerLookUp, lookUpCellMatcher, targetColHeaderLookup)
                 .findFirst()
                 .orElseThrow(NotFoundException::new)
                 .getText()
