ccee9c0d41212e02b0714ab3be3bc37a44a53b79
Graham Russell <graham@ham1.co.uk>
2020-11-01 19:09:11 +0000

    Simplify ChromeImpl and update .travis.yml to pass (#3)

--- src/main/java/com/frameworkium/lite/ui/driver/drivers/ChromeImpl.java
+++ src/main/java/com/frameworkium/lite/ui/driver/drivers/ChromeImpl.java
@@ -2,33 +2,16 @@ package com.frameworkium.lite.ui.driver.drivers;
 
 import com.frameworkium.lite.common.properties.Property;
 import com.frameworkium.lite.ui.driver.AbstractDriver;
-import com.google.common.collect.ImmutableMap;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.util.Collections;
 
 public class ChromeImpl extends AbstractDriver {
 
     @Override
     public ChromeOptions getCapabilities() {
         ChromeOptions chromeOptions = new ChromeOptions();
-
-        // useful defaults
-        chromeOptions.setCapability(
-                "chrome.switches",
-                Collections.singletonList("--no-default-browser-check"));
-        chromeOptions.setCapability(
-                "chrome.prefs",
-                ImmutableMap.of("profile.password_manager_enabled", "false"));
-
-        // Workaround Docker/Travis issue
-        if (Boolean.parseBoolean(System.getenv("CHROME_NO_SANDBOX"))) {
-            chromeOptions.addArguments("--no-sandbox");
-        }
-
         chromeOptions.setHeadless(Property.HEADLESS.getBoolean());
         return chromeOptions;
     }
