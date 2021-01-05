c7d99b219f5e372ce6a7f3f6478434e13abad9db
MJ <Mjl33@users.noreply.github.com>
2021-01-05 17:30:13 +0800

    Create maven.yml (#157)
    * Migrate to github actions CI

--- src/test/java/com/frameworkium/integration/seleniumhq/pages/HomePage.java
+++ src/test/java/com/frameworkium/integration/seleniumhq/pages/HomePage.java
@@ -4,13 +4,18 @@ import com.frameworkium.core.ui.annotations.Visible;
 import com.frameworkium.core.ui.pages.BasePage;
 import com.frameworkium.core.ui.pages.PageFactory;
 import com.frameworkium.integration.seleniumhq.components.HeaderComponent;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage<HomePage> {
 
     @CacheLookup
     @Visible
     private HeaderComponent header;
+
+    @FindBy(css = ".headerLink + #dropdownButton")
+    private WebElement menuLink;
 
     public static HomePage open() {
         return PageFactory.newInstance(
@@ -19,6 +24,10 @@ public class HomePage extends BasePage<HomePage> {
     }
 
     public HeaderComponent getHeader() {
+        // when using Selenium grid, the browser size is smaller and the header is hidden behind this link
+        if (menuLink.isDisplayed()) {
+            menuLink.click();
+        }
         return header;
     }
 }
