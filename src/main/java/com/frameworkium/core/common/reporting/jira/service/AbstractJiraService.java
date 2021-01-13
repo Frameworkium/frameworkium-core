ab84d8fbf526c7570d1a2ed1e5948dfa6021d7a8
MJ <Mjl33@users.noreply.github.com>
2021-01-13 16:48:50 +0800

    Removed unused jackson dependencies (#203)
    Co-authored-by: MJL33 <>

--- src/main/java/com/frameworkium/core/common/reporting/jira/service/AbstractJiraService.java
+++ src/main/java/com/frameworkium/core/common/reporting/jira/service/AbstractJiraService.java
@@ -1,15 +1,9 @@
 package com.frameworkium.core.common.reporting.jira.service;
 
-import com.fasterxml.jackson.annotation.*;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.frameworkium.core.api.services.BaseService;
 import com.frameworkium.core.common.properties.Property;
 import com.frameworkium.core.common.reporting.jira.endpoint.JiraEndpoint;
 import io.restassured.RestAssured;
-import io.restassured.config.ObjectMapperConfig;
-import io.restassured.config.RestAssuredConfig;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
@@ -20,7 +14,6 @@ public class AbstractJiraService extends BaseService {
         return RestAssured.given()
                 .baseUri(JiraEndpoint.BASE_URI.getUrl())
                 .relaxedHTTPSValidation()
-                .config(config())
                 .auth().preemptive().basic(
                         Property.JIRA_USERNAME.getValue(),
                         Property.JIRA_PASSWORD.getValue());
@@ -28,24 +21,6 @@ public class AbstractJiraService extends BaseService {
 
     @Override
     protected ResponseSpecification getResponseSpec() {
-        return null;
+        throw new AssertionError("Unimplemented");
     }
-
-    private RestAssuredConfig config() {
-        return RestAssuredConfig.config().objectMapperConfig(
-                ObjectMapperConfig.objectMapperConfig().jackson2ObjectMapperFactory(
-                        (type, s) -> {
-                            final ObjectMapper objectMapper = new ObjectMapper();
-                            objectMapper.registerModule(new JavaTimeModule());
-                            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-                            objectMapper.setVisibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE);
-                            objectMapper.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
-                            objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-                            objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-                            return objectMapper;
-                        }
-                )
-        );
-    }
-
 }
