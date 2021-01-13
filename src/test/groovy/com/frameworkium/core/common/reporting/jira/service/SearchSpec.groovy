759858a4f8f2ffe7bbbb018c8382880ca181a27b
MJ <Mjl33@users.noreply.github.com>
2021-01-13 13:21:01 +0800

    Refactor jira API and add unitttest (#200)
    * Refactor jira API 
     -  Search jira issue
     - Edit field 
     - Add comment
    
    Co-authored-by: MJL33 <>

--- src/test/groovy/com/frameworkium/core/common/reporting/jira/service/SearchSpec.groovy
+++ src/test/groovy/com/frameworkium/core/common/reporting/jira/service/SearchSpec.groovy
@@ -3,16 +3,16 @@ package com.frameworkium.core.common.reporting.jira.service
 
 import spock.lang.Specification
 
-class SearchServiceSpec extends Specification {
+class SearchSpec extends Specification {
     def "Getting keys from JQL JIRA search"() {
         given:
             // todo - parameterize jiraURL
-            System.properties["jiraURL"] = "http://localhost:8080/jira/rest/api/2"
+            System.properties["jiraURL"] = "http://localhost:8080"
             System.properties["jiraUsername"] = "username"
             System.properties["jiraPassword"] = "password"
         when:
             //wiremock stub (jiraSearchServiceSpec.json) - ensure that the query matches in the stub
-            def searchService = new SearchService("searchServiceSpec")
+            def searchService = new Search("searchServiceSpec")
         then:
             searchService.getKeys() == ["KEY-1", "KEY-2"]
     }
