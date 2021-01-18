e80a9d3a0d2e71fb2b9517d4d982c13aec9fa4b0
MJL33 <>
2021-01-18 17:34:19 +0800

    Fix Zephyr execution endpoint

--- src/main/java/com/frameworkium/core/common/reporting/jira/service/Execution.java
+++ src/main/java/com/frameworkium/core/common/reporting/jira/service/Execution.java
@@ -1,18 +1,16 @@
 package com.frameworkium.core.common.reporting.jira.service;
 
 import com.frameworkium.core.common.reporting.jira.dto.execution.AddTestToCycleOperationDto;
+import com.frameworkium.core.common.reporting.jira.endpoint.ZephyrEndpoint;
 
-import static com.frameworkium.core.common.reporting.jira.JiraConfig.REST_ZAPI_PATH;
 import static org.apache.http.HttpStatus.SC_OK;
 
 public class Execution extends AbstractJiraService {
-    private static final String EXECUTION_REST_PATH = REST_ZAPI_PATH + "execution";
-
     public String addTestsToCycle(AddTestToCycleOperationDto addTestToCycleOperationDto) {
         return getRequestSpec()
-                .basePath(EXECUTION_REST_PATH)
+                .basePath(ZephyrEndpoint.ADD_TEST_TO_EXECUTION.getUrl())
                 .body(addTestToCycleOperationDto)
-                .post("/addTestsToCycle")
+                .post()
                 .then()
                 .log().ifValidationFails()
                 .statusCode(SC_OK)
