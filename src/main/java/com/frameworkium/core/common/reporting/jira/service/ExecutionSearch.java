aed3d8923678007ba32d99279857d94abb8981b7
MJL33 <>
2021-01-19 17:34:36 +0800

    Fix checkstyle

--- src/main/java/com/frameworkium/core/common/reporting/jira/service/ExecutionSearch.java
+++ src/main/java/com/frameworkium/core/common/reporting/jira/service/ExecutionSearch.java
@@ -14,10 +14,18 @@ public class ExecutionSearch extends AbstractJiraService {
                 .basePath(ZephyrEndpoint.EXECUTE_SEARCH.getUrl())
                 .queryParam("zqlQuery", zqlQuery);
 
-        if (filterId != null) reqspec.queryParam("filterId", filterId);
-        if (offset != null) reqspec.queryParam("offset", offset);
-        if (maxRecords != null) reqspec.queryParam("maxRecords", maxRecords);
-        if (expand != null) reqspec.queryParam("expand", expand);
+        if (filterId != null) {
+            reqspec.queryParam("filterId", filterId);
+        }
+        if (offset != null) {
+            reqspec.queryParam("offset", offset);
+        }
+        if (maxRecords != null) {
+            reqspec.queryParam("maxRecords", maxRecords);
+        }
+        if (expand != null) {
+            reqspec.queryParam("expand", expand);
+        }
 
         return reqspec.when()
                 .get().then().log().ifValidationFails()
