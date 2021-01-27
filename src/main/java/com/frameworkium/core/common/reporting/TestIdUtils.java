86d4d34934d30d5789978161fbd4419e4e38e377
MJ <Mjl33@users.noreply.github.com>
2021-01-27 15:29:16 +0800

    Add utility for multiple @TmsLink or @Issue per test method support (#226)
    * Add utility for multiple @TmsLink or @Issue per test method support
    
    Co-authored-by: MJL33 <>

--- src/main/java/com/frameworkium/core/common/reporting/TestIdUtils.java
+++ src/main/java/com/frameworkium/core/common/reporting/TestIdUtils.java
@@ -5,7 +5,9 @@ import io.qameta.allure.TmsLink;
 import org.testng.IMethodInstance;
 
 import java.lang.reflect.Method;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Objects.nonNull;
 
@@ -22,7 +24,10 @@ public class TestIdUtils {
      * @return Optional of either the {@link TmsLink} or {@link Issue} value.
      * @throws IllegalStateException if {@link TmsLink} and {@link Issue}
      *                               are specified inconstantly.
+     * @deprecated Use
+     * {@link com.frameworkium.core.common.reporting.TestIdUtils#getIssueOrTmsLinkValues(IMethodInstance)} instead
      */
+    @Deprecated
     public static Optional<String> getIssueOrTmsLinkValue(IMethodInstance iMethod) {
         Method method = iMethod.getMethod().getConstructorOrMethod().getMethod();
         return getIssueOrTmsLinkValue(method);
@@ -34,7 +39,10 @@ public class TestIdUtils {
      *
      * @param method the method to check for test ID annotations.
      * @return Optional of the {@link TmsLink} or {@link Issue} value.
+     * @deprecated Use
+     * {@link com.frameworkium.core.common.reporting.TestIdUtils#getIssueOrTmsLinkValues(Method)} instead.
      */
+    @Deprecated
     public static Optional<String> getIssueOrTmsLinkValue(Method method) {
         TmsLink tcIdAnnotation = method.getAnnotation(TmsLink.class);
         Issue issueAnnotation = method.getAnnotation(Issue.class);
@@ -46,5 +54,37 @@ public class TestIdUtils {
         } else {
             return Optional.empty();
         }
+    }
+
+    /**
+     * Get list of {@link TmsLink} or {@link Issue}.
+     *
+     * @param iMethod the {@link IMethodInstance} to check for test ID annotations.
+     * @return List of either the {@link TmsLink} or {@link Issue} value.
+     * @throws IllegalStateException if {@link TmsLink} and {@link Issue}
+     *                               are specified inconstantly.
+     */
+    public static List<String> getIssueOrTmsLinkValues(IMethodInstance iMethod) {
+        Method method = iMethod.getMethod().getConstructorOrMethod().getMethod();
+        return getIssueOrTmsLinkValues(method);
+    }
+
+    /**
+     * Get a list of {@link TmsLink} or {@link Issue} for a method.
+     * If both are specified it will return just the list of {@link TmsLink} values.
+     *
+     * @param method the method to check for test Id annotations.
+     * @return List of {@link TmsLink} or {@link Issue} values.
+     */
+    public static List<String> getIssueOrTmsLinkValues(Method method) {
+        TmsLink[] tcIdAnnotations = method.getAnnotationsByType(TmsLink.class);
+        Issue[] issueAnnotations = method.getAnnotationsByType(Issue.class);
+        if (tcIdAnnotations.length > 0) {
+            return Stream.of(tcIdAnnotations).map(TmsLink::value).collect(Collectors.toList());
+        }
+        if (issueAnnotations.length > 0) {
+            return Stream.of(issueAnnotations).map(Issue::value).collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 }
--- src/test/groovy/com/frameworkium/core/common/reporting/TestIdUtilsSpec.groovy
+++ src/test/groovy/com/frameworkium/core/common/reporting/TestIdUtilsSpec.groovy
@@ -15,12 +15,33 @@ class TestIdUtilsSpec extends Specification {
         then:
             value == expectedValue
         where:
-            methodName      | expectedValue
-            "none"          | Optional.empty()
-            "tmsLink"       | Optional.of("TMSLink")
-            "issue"         | Optional.of("ISSUE")
-            "bothSame"      | Optional.of("SAME")
-            "bothDifferent" | Optional.of("TMSLink")
+            methodName                | expectedValue
+            "none"                    | Optional.empty()
+            "tmsLink"                 | Optional.of("TMSLink")
+            "issue"                   | Optional.of("ISSUE")
+            "bothSame"                | Optional.of("SAME")
+            "bothDifferent"           | Optional.of("TMSLink")
+            "multipleTmsLink"         | Optional.empty()
+            "multipleTmsLinkAndIssue" | Optional.empty()
+            "multipleIssue"           | Optional.empty()
+    }
+
+    def "GetIssueOrTmsLinkValues get TmsLink or Issue values for method #methodName"() {
+        when:
+            def value = TestIdUtils.getIssueOrTmsLinkValues(
+                    TestIdData.getMethod(methodName))
+        then:
+            value == expectedValue
+        where:
+            methodName                | expectedValue
+            "none"                    | []
+            "tmsLink"                 | ["TMSLink"]
+            "issue"                   | ["ISSUE"]
+            "bothSame"                | ["SAME"]
+            "bothDifferent"           | ["TMSLink"]
+            "multipleTmsLink"         | ["TMSLink1", "TMSLink2"]
+            "multipleTmsLinkAndIssue" | ["TMSLink3", "TMSLink4"]
+            "multipleIssue"           | ["Issue3", "Issue4"]
     }
 
     class TestIdData {
@@ -40,6 +61,20 @@ class TestIdUtilsSpec extends Specification {
         @TmsLink("TMSLink")
         @Issue("ISSUE")
         void bothDifferent() {}
+
+        @TmsLink("TMSLink1")
+        @TmsLink("TMSLink2")
+        void multipleTmsLink() {}
+
+        @Issue("Issue1")
+        @Issue("Issue2")
+        @TmsLink("TMSLink3")
+        @TmsLink("TMSLink4")
+        void multipleTmsLinkAndIssue() {}
+
+        @Issue("Issue3")
+        @Issue("Issue4")
+        void multipleIssue() {}
     }
 }
 
