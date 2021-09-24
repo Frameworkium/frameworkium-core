522c712918eb4455f458c02bc48959e239d2886d
MJL33 <>
2021-09-24 22:55:13 +0800

    Update SeleniumPage Page

--- src/test/java/com/frameworkium/integration/seleniumhq/components/HeaderComponent.java
+++ src/test/java/com/frameworkium/integration/seleniumhq/components/HeaderComponent.java
@@ -2,9 +2,11 @@ package com.frameworkium.integration.seleniumhq.components;
 
 import com.frameworkium.core.htmlelements.element.HtmlElement;
 import com.frameworkium.core.htmlelements.element.Link;
+import com.frameworkium.core.ui.UITestLifecycle;
 import com.frameworkium.core.ui.pages.PageFactory;
 import com.frameworkium.integration.seleniumhq.pages.SeleniumDownloadPage;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
 @FindBy(className = "navbar")
@@ -14,6 +16,7 @@ public class HeaderComponent extends HtmlElement {
     private Link downloadLink;
 
     public SeleniumDownloadPage clickDownloadLink() {
+        UITestLifecycle.get().getWait().until(ExpectedConditions.elementToBeClickable(downloadLink));
         downloadLink.click();
         return PageFactory.newInstance(SeleniumDownloadPage.class);
     }
