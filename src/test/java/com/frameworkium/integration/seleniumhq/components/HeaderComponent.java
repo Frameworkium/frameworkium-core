81daacbc021fbc61c8dde141115d7f8cc89b8b07
MJ <Mjl33@users.noreply.github.com>
2021-09-24 23:06:26 +0800

    Remove capture from CI (#290)
    * Remove capture from CI
    
    * Update FrameworkiumBugsTest
    
    * Update SeleniumPage Page
    
    * Update SeleniumPage Page
    
    Co-authored-by: MJL33 <>

--- src/test/java/com/frameworkium/integration/seleniumhq/components/HeaderComponent.java
+++ src/test/java/com/frameworkium/integration/seleniumhq/components/HeaderComponent.java
@@ -2,18 +2,21 @@ package com.frameworkium.integration.seleniumhq.components;
 
 import com.frameworkium.core.htmlelements.element.HtmlElement;
 import com.frameworkium.core.htmlelements.element.Link;
+import com.frameworkium.core.ui.UITestLifecycle;
 import com.frameworkium.core.ui.pages.PageFactory;
 import com.frameworkium.integration.seleniumhq.pages.SeleniumDownloadPage;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 
-@FindBy(id = "header")
+@FindBy(className = "navbar")
 public class HeaderComponent extends HtmlElement {
 
-    @FindBy(css = "#navbar > a:nth-child(3)")
+    @FindBy(css = "#main_navbar [href='/downloads']")
     private Link downloadLink;
 
     public SeleniumDownloadPage clickDownloadLink() {
+        UITestLifecycle.get().getWait().until(ExpectedConditions.elementToBeClickable(downloadLink));
         downloadLink.click();
         return PageFactory.newInstance(SeleniumDownloadPage.class);
     }
